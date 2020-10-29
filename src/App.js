2b5134a994f1427ae786586d132676de07229316
Fedor <screenshot@inbox.ru>
2020-10-29 20:00:35 +0700

    Add loader

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Loader from './Loader/Loader';
 
 class App extends Component {
   async componentDidMount() {
@@ -11,7 +12,7 @@ class App extends Component {
   render() {
     return (
       <div className='container'>
-        <div>12312223121112</div>
+        <Loader />
       </div>
     );
   }
