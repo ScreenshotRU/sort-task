7917ac21d1dc5e91082811bb3a2b82fc1f895b23
Fedor <screenshot@inbox.ru>
2020-10-29 19:50:41 +0700

    Add data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-function App() {
-  return (
-    <div className='App'>
-      <div>12312312</div>
-    </div>
-  );
+import React, { Component } from 'react';
+
+class App extends Component {
+  async componentDidMount() {
+    const response = await fetch(
+      `http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`
+    );
+    const data = await response.json();
+    console.log(data);
+  }
+  render() {
+    return (
+      <div className='container'>
+        <div>12312223121112</div>
+      </div>
+    );
+  }
 }
 
 export default App;
